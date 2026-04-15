69b6bb3f95346ee9cb97e75f40962e4d27c80473
Taras Pokornyy <taras.pokornyy@datarobot.com>
2026-04-15 14:58:17 +0300

    [CFX_5610] Update the copyright year

--- cmd/self/completion/install/cmd_test.go
+++ cmd/self/completion/install/cmd_test.go
@@ -1,4 +1,4 @@
-// Copyright 2025 DataRobot, Inc. and its affiliates.
+// Copyright 2026 DataRobot, Inc. and its affiliates.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
